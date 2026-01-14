edfae1fd7491093b406c8ae37b184fbfb610c12a
saifiimuhammad <muhammadsaifarain786@gmail.com>
2026-01-14 19:05:03 +0500

    ready for deployment

--- src/config.js
+++ src/config.js
@@ -2,14 +2,14 @@
 //     baseUrl: "http://localhost:4000/api/v1",
 //     GOOGLE_API_KEY: "AIzaSyDo4GPTF9dChnFkV-uX5zoiA7JHZongxPI"
 // }
-// const config = {
-//   baseUrl: "http://localhost:3000/api/v1",
-//   GOOGLE_API_KEY: "AIzaSyA37GH1PD5_UId3KXqp_MwkPQOGZv_TOv0",
-// };
-
 const config = {
-  baseUrl: "https://lorepa-backend.vercel.app/api/v1",
-  GOOGLE_API_KEY: "AIzaSyDo4GPTF9dChnFkV-uX5zoiA7JHZongxPI",
+  baseUrl: "https://lorepa-backend.onrender.com/api/v1",
+  GOOGLE_API_KEY: "AIzaSyA37GH1PD5_UId3KXqp_MwkPQOGZv_TOv0",
 };
+
+// const config = {
+//   baseUrl: "https://lorepa-backend.vercel.app/api/v1",
+//   GOOGLE_API_KEY: "AIzaSyDo4GPTF9dChnFkV-uX5zoiA7JHZongxPI",
+// };
 
 export default config;
