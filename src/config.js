5ee504161389f77b622063b1f7f3ccdff0307953
saifiimuhammad <muhammadsaifarain786@gmail.com>
2026-01-14 11:29:33 +0500

    Fixed the production config

--- src/config.js
+++ src/config.js
@@ -2,9 +2,14 @@
 //     baseUrl: "http://localhost:4000/api/v1",
 //     GOOGLE_API_KEY: "AIzaSyDo4GPTF9dChnFkV-uX5zoiA7JHZongxPI"
 // }
+// const config = {
+//   baseUrl: "http://localhost:3000/api/v1",
+//   GOOGLE_API_KEY: "AIzaSyA37GH1PD5_UId3KXqp_MwkPQOGZv_TOv0",
+// };
+
 const config = {
-  baseUrl: "http://localhost:3000/api/v1",
-  GOOGLE_API_KEY: "AIzaSyA37GH1PD5_UId3KXqp_MwkPQOGZv_TOv0",
+  baseUrl: "https://lorepa-backend.vercel.app/api/v1",
+  GOOGLE_API_KEY: "AIzaSyDo4GPTF9dChnFkV-uX5zoiA7JHZongxPI",
 };
 
 export default config;
